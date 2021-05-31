4d8df386ab0b9afb4da26538fb250cf991f82f67
pavan <76779570+PavanVishwa@users.noreply.github.com>
2021-05-31 14:21:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(500,500);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function Take_Photo(){
+    save('My_photo_2')
+}
